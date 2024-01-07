1618cc9791f1f541785eaf91fe81862f493e6f2e
Melanie H <mlh2197@columbia.edu>
2024-01-07 14:19:45 -0500

    chore(pdf-viewer): configure plugin.xml file, create JS file.

---
+++ www/pdf-viewer.js
@@ -0,0 +1,17 @@
+const exec = require('cordova/exec');
+
+const pdfViewer = {};
+
+viewer.openPDF = function(url, successFn, errorFn) {
+    if (!url) {
+        throw new Error("A non-empty url is required.");
+    }
+
+    if (typeof url !== 'string') {
+        throw new Error('A string url is required.');
+    }
+
+    exec(successFn, errorFn, 'PDFViewer', 'openPDFWithURL', [url]);
+};
+
+module.exports = viewer;
